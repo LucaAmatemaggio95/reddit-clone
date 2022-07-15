ff5886841228b5f121eecdf3e5de0fd29b223264
Luca Amatemaggio <lamatemaggio@gmail.com>
2022-07-15 09:51:50 +0200

    Created delete comments functinality

--- src/components/Posts/Comments/Comments.tsx
+++ src/components/Posts/Comments/Comments.tsx
@@ -41,6 +41,7 @@ const Comments: React.FC<CommentsProps> = ({
   const [comments, setComments] = useState<Comment[]>([]);
   const [fetchLoading, setFetchLoading] = useState(true);
   const [createLoading, setCreateLoading] = useState(false);
+  const [loadingDeleteId, setLoadingDeleteId] = useState("");
   const setPostState = useSetRecoilState(postState);
 
   const onCreateComment = async () => {
@@ -90,17 +91,48 @@ const Comments: React.FC<CommentsProps> = ({
     setCreateLoading(false);
   };
 
-  const onDeleteComment = async (comment: any) => {
-    // delete comment document
-    // update post numberOfComments -1
-    // update client recoil state
+  const onDeleteComment = async (comment: Comment) => {
+    setLoadingDeleteId(comment.id);
+
+    try {
+      const batch = writeBatch(firestore);
+
+      // delete comment document
+      const commentDocRef = doc(firestore, "comments", comment.id);
+
+      batch.delete(commentDocRef);
+
+      const postDocRef = doc(firestore, "posts", selectedPost?.id!);
+
+      // update post numberOfComments -1
+      batch.update(postDocRef, {
+        numberOfComments: increment(-1)
+      });
+
+      await batch.commit();
+
+      // update client recoil state
+      setPostState(prev => ({
+        ...prev,
+        selectedPost: {
+          ...prev.selectedPost,
+          numberOfComments: prev.selectedPost?.numberOfComments! - 1
+        } as Post
+      }));
+
+      setComments(prev => prev.filter(item => item.id !== comment.id));
+    } catch (error) {
+      console.log("onDeleteComment", error);
+    }
+
+    setLoadingDeleteId("");
   };
 
   const getPostComments = async () => {
     try {
       const commentsQuery = query(
         collection(firestore, "comments"),
-        where("postiId", "==", selectedPost?.id),
+        where("postId", "==", selectedPost?.id),
         orderBy("createdAt", "desc")
       );
 
@@ -173,7 +205,7 @@ const Comments: React.FC<CommentsProps> = ({
                     key={comment.id}
                     comment={comment}
                     onDeleteComment={onDeleteComment}
-                    loadingDelete={false}
+                    loadingDelete={loadingDeleteId === comment.id}
                     userId={user.uid}
                   />
                 ))}
